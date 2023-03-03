016c2867f3ced309994a3cb1bac27dba58132618
daohoangson123 <108396570+daohoangson123@users.noreply.github.com>
2023-03-03 15:57:47 +0700

    fixed user form

--- src/components/FUNCTIONPAGES/User.jsx
+++ src/components/FUNCTIONPAGES/User.jsx
@@ -24,10 +24,14 @@ const User = ({...props}) => {
             <div className='SignIn'>
                 <form action="">
                         <div className='Input_Container'>
-                            <label htmlFor="UserName">User Name:</label>
-                            <input type="text" name="UserName" id="UserName" value={inputValue.UserName} required autoComplete='off' onChange={(event) => setInputValue({...inputValue, 'UserName' : event.target.value })}/>
-                            <label htmlFor="UserPass">Password:</label>
-                            <input type="password" name="UserPass" id="UserPass" value={inputValue.Password} required autoComplete='off' onChange={(event) => setInputValue({...inputValue, 'Password' : event.target.value })}/>
+                            <div>
+                                <i className="fa-solid fa-user"></i>
+                                <input type="text" name="UserName" id="UserName" value={inputValue.UserName} required autoComplete='off' placeholder='User Name' onChange={(event) => setInputValue({...inputValue, 'UserName' : event.target.value })}/>
+                            </div>
+                            <div>
+                                <i className="fa-solid fa-lock"></i>
+                                <input type="password" name="UserPass" id="UserPass" value={inputValue.Password} required autoComplete='off' placeholder='Password' onChange={(event) => setInputValue({...inputValue, 'Password' : event.target.value })}/>
+                            </div>
                         </div>
                         <div className='User_Submit'>
                             <button type="submit" value="Submit">
@@ -43,12 +47,18 @@ const User = ({...props}) => {
             <div className='Regis'>
                 <form action="">
                         <div className='Input_Container'>
-                            <label htmlFor="RegisEmail">Email:</label>
-                            <input type="email" name="RegisEmail" id="RegisEmail" value={inputValue.RegisEmail} required autoComplete='off' onChange={(event) => setInputValue({...inputValue, 'RegisEmail' : event.target.value })}/>
-                            <label htmlFor="RegisUser">User Name:</label>
-                            <input type="text" name="RegisUser" id="RegisUser" value={inputValue.RegisUser} required autoComplete='off' onChange={(event) => setInputValue({...inputValue, 'RegisUser' : event.target.value })}/>
-                            <label htmlFor="RegisPass">Password:</label>
-                            <input type="password" name="RegisPass" id="RegisPass" value={inputValue.RegisPass} required autoComplete='off' onChange={(event) => setInputValue({...inputValue, 'RegisPass' : event.target.value })}/>
+                            <div>
+                                <i className="fa-solid fa-envelope"></i>
+                                <input type="email" name="RegisEmail" id="RegisEmail" value={inputValue.RegisEmail} required autoComplete='off' placeholder='Email' onChange={(event) => setInputValue({...inputValue, 'RegisEmail' : event.target.value })}/>
+                            </div>
+                            <div>
+                                <i className="fa-solid fa-user-plus"></i>
+                                <input type="text" name="RegisUser" id="RegisUser" value={inputValue.RegisUser} required autoComplete='off' placeholder='User Name' onChange={(event) => setInputValue({...inputValue, 'RegisUser' : event.target.value })}/>
+                            </div>
+                            <div>
+                                <i className="fa-solid fa-lock"></i>
+                                <input type="password" name="RegisPass" id="RegisPass" value={inputValue.RegisPass} required autoComplete='off' placeholder='Password' onChange={(event) => setInputValue({...inputValue, 'RegisPass' : event.target.value })}/>
+                            </div>
                         </div>
                         <div className='User_Submit'>
                             <button type="submit" value="Submit">
