75035dedcbf5a0577cdb63d5ec13254defd45341
daohoangson123 <108396570+daohoangson123@users.noreply.github.com>
2024-02-25 23:33:15 +0700

    minor fix

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -8,45 +8,10 @@ import { pageAccessedByReload } from '../../../data/isPageReloaded';
 import { useEffect, useState } from 'react';
 
 const Header = ({ isSignIn }) => {
-    const [isUp, setIsUp] = useState(true);
-    const [isDown, setIsDown] = useState(false);
-
-    useEffect(() => {
-        let lastScroll = window.screenY;
-
-        function checkDown() {
-            let currentScroll = window.screenY;
-            if (currentScroll > lastScroll) {
-                setIsUp(false);
-                setIsDown(true);
-                window.addEventListener('scroll', checkUp);
-                window.removeEventListener('scroll', checkDown);
-            }
-            lastScroll = currentScroll <= 0 ? 0 : currentScroll;
-        }
-
-        function checkUp() {
-            let currentScroll = window.scrollY;
-            if (currentScroll < lastScroll) {
-                setIsUp(true);
-                setIsDown(false);
-                window.addEventListener('scroll', checkDown);
-                window.removeEventListener('scroll', checkUp);
-            }
-            lastScroll = currentScroll <= 0 ? 0 : currentScroll;
-        }
-
-        window.addEventListener('scroll', checkDown);
-        window.addEventListener('scroll', checkUp);
-
-        console.log('run');
-    }, [isUp]);
-
     return (
         <header
             style={{
                 animation: pageAccessedByReload && 'none',
-                top: isDown ? '-60px' : 0,
             }}>
             <SignBar isSignIn={isSignIn} />
             <NavBar />
