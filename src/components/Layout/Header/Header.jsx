814ead63f924977de17650bf12f49eab62ab7588
daohoangson123 <108396570+daohoangson123@users.noreply.github.com>
2024-02-26 19:47:47 +0700

    minor fix

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -5,12 +5,40 @@ import NavBar from './NavBar/NavBar';
 //
 import { pageAccessedByReload } from '../../../data/isPageReloaded';
 //
+import { useState, useCallback, useEffect } from 'react';
 
 const Header = ({ isSignIn }) => {
+    const [y, setY] = useState(window.scrollY);
+    const [isDown, setIsDown] = useState();
+
+    const handleNavigation = useCallback(
+        (e) => {
+            const window = e.currentTarget;
+            if (y > window.scrollY && y - window.scrollY > 35) {
+                setIsDown(false);
+            } else if (y < window.scrollY && window.scrollY - y > 35) {
+                setIsDown(true);
+            }
+            setY(window.scrollY);
+        },
+        [y],
+    );
+
+    useEffect(() => {
+        setY(window.scrollY);
+
+        window.addEventListener('scroll', handleNavigation);
+
+        return () => {
+            window.removeEventListener('scroll', handleNavigation);
+        };
+    }, [handleNavigation]);
+
     return (
         <header
             style={{
                 animation: pageAccessedByReload && 'none',
+                top: isDown ? '-60px' : 0,
             }}>
             <SignBar isSignIn={isSignIn} />
             <NavBar />
